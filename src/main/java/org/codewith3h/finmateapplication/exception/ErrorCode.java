ad3c217a6d2963a660c5348d83d04bd103a09fe1
Huy Hoang <hhuy0405@gmail.com>
2025-06-09 13:12:17 +0700

    commit budget management feature

--- src/main/java/org/codewith3h/finmateapplication/exception/ErrorCode.java
+++ src/main/java/org/codewith3h/finmateapplication/exception/ErrorCode.java
@@ -8,11 +8,11 @@ public enum ErrorCode {
     USER_ID_IS_REQUIRED(1004, "User id is required"),
     AMOUNT_MUST_BE_POSITIVE(1005, "Amount must be greater than 0"),
     GOAL_ID_IS_REQUIRED(1006, "Goal id is required"),
-    TRANSACTION_NOT_FOUND_EXCEPTION(1001, "Transaction not found"),
-    CATEGORY_NOT_FOUND_EXCEPTION(1003, "Category not found"),
-    INVALID_FREQUENCY_EXCEPTION(1004, "Invalid Frequency"),
-    INVALID_INPUT(1001, "Invalid input data"),
-    BUDGET_EXISTS(1002, "Budget already exists"),
+    TRANSACTION_NOT_FOUND_EXCEPTION(1007, "Transaction not found"),
+    CATEGORY_NOT_FOUND_EXCEPTION(1008, "Category not found"),
+    INVALID_FREQUENCY_EXCEPTION(1009, "Invalid Frequency"),
+    INVALID_INPUT(1010, "Invalid input data"),
+    BUDGET_EXISTS(1011, "Budget already exists"),
     ;
 
 
--- src/main/java/org/codewith3h/finmateapplication/repository/TransactionRepository.java
+++ src/main/java/org/codewith3h/finmateapplication/repository/TransactionRepository.java
@@ -1,14 +1,23 @@
 package org.codewith3h.finmateapplication.repository;
 
 import org.codewith3h.finmateapplication.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     Page<Transaction> findByUserId(Integer userId, Pageable pageable);
 
-    Optional<Transaction> findByIdAndUserId(Integer transactionId,  Integer userId);
+    Optional<Transaction> findByIdAndUserId(Integer transactionId, Integer userId);
 
     Page<Transaction> findByUserIdAndTransactionDateBetween(Integer userId, LocalDate startDate, LocalDate endDate, Pageable pageable);
 
